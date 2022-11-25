063ac751825b92d2087337e0ebbca5943c8d2bc3
YuSo666 <julsi.08@bk.ru>
2022-11-25 23:20:42 +0800

    JSON, margin

--- dz8.js
+++ dz8.js
@@ -1,56 +1,63 @@
 //1. объекты для еды
 let dough = {
+    name: 'dough',
     price: 200
 };
 
 let chiken = {
+    name: 'chiken',
     price: 450
 };
 
 let beef = {
+    name: 'beef',
     price: 480
 };
 
 let mutton = {
+    name: 'mutton',
     price: 460
 };
 
 let cheese = {
+    name: 'cheese',
     price: 260
 };
 
 let sauce = {
+    name: 'sauce',
     price: 180
 };
 
 //2. блюда и их ингредиенты
 let buuzy = {
-    ingredients: ['dough', 'beef', 'mutton'],
-    price: 48
+    ingredients: [dough.name, beef.name, mutton.name],
+    margin: 1.3
 }
+alert(JSON.stringify(buuzy.ingredients))
 
 let koreanChiken = {
-    ingredients: ['chiken', 'sauce'],
-    price: 380
+    ingredients: [chiken.name, sauce.name],
+    margin: 1.6
 }
+alert(JSON.stringify(koreanChiken.ingredients))
 
 let pizza = {
-    ingredients: ['dough', 'chiken', 'cheese'],
-    price: 520
+    ingredients: [dough.name, chiken.name, cheese.name],
+    margin: 1.3
 }
 
 
 //3. удалим buuzy из массива food
-let food = [
-    'buuzy', 'koreanChiken', 'pizza'
-];
+let food = [buuzy, koreanChiken, pizza];
 
 food.shift(0);
-alert (food);
+alert (JSON.stringify(food));
 
 //4. Добавить позы обратно и на первое место, 
-food.unshift('buuzy');
-alert (food);
+food.unshift(buuzy);
+alert (JSON.stringify(food));
+
 //но удалить из поз баранину
 buuzy.ingredients.splice(2,1);
 alert (buuzy.ingredients);
@@ -59,7 +66,7 @@ alert (buuzy.ingredients);
 let menuBuuzy=dough.price+beef.price;
 alert (menuBuuzy); //сколько потратили на buuzy
 
-let priceBuuzy=menuBuuzy*1.3
+let priceBuuzy=menuBuuzy*buuzy.margin
 alert(priceBuuzy) //за сколько буду продавать
 
 //сколько заработаем на каждом блюде
